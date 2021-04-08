7674509c05f06dd3699bd3bc76e8cb6a716fce7c
384219749 <384219749@qq.com>
2021-04-08 22:35:13 +0800

    添加data模块

---
+++ my-jdbc/src/test/java/com/wyg/myjdbc/MyJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyg.myjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
