8e64c61cec47ac51b17ed15a6cf433467f00daeb
384219749 <384219749@qq.com>
2021-04-13 22:02:14 +0800

    提交mybatis相关配置

--- my-data/src/main/java/com/wyg/mydata/MyDataApplication.java
+++ my-data/src/main/java/com/wyg/mydata/MyDataApplication.java
@@ -1,9 +1,11 @@
 package com.wyg.mydata;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.wyg.mydata.mapper")
 public class MyDataApplication {
 
 	public static void main(String[] args) {
---
+++ my-data/src/main/java/com/wyg/mydata/vo/DeptVO.java
@@ -0,0 +1,9 @@
+package com.wyg.mydata.vo;
+
+import lombok.Data;
+
+@Data
+public class DeptVO {
+    private String deptId;
+    private String deptName;
+}
